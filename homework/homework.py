6dbc2ebad1e6af8ef300adc7344b6d8e0d5f3fee
joytu48-sudo <joytu48@gmail.com>
2026-07-12 11:35:34 +0600

    things i do

--- homework/homework.py
+++ homework/homework.py
@@ -1,2 +1,6 @@
 #things i do
-print("wake up on either 8am or 1pm")
+print("wake up on either 8am or 1pm[depends on which day it is]\n")
+print("eat my breakfast[simple aloo bhorta with ruti, delicious]\n")
+print("go to school[depends on what day it is]\n")
+print("play games, online or offline[if i find anything worth while]\n")
+print("total time i study[if i can]:",2*7)
