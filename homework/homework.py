5e09844e3d428bb1b7022f204ad35dd3ab23ba60
joytu48-sudo <joytu48@gmail.com>
2026-07-12 11:17:08 +0600

    keyword

---
+++ homework/homework.py
@@ -0,0 +1,2 @@
+#things i do
+print("wake up on either 8am or 1pm")
---
+++ module1/lesson2/activity3.py
@@ -0,0 +1,6 @@
+import keyword
+
+#print the list of keyword
+print(keyword.kwlist)
+#is it a keyword or not
+print(keyword.iskeyword("False"))
