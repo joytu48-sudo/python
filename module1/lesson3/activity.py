be3c71857a2ca5838d1dfcf5da658a8473aa4830
joytu48-sudo <joytu48@gmail.com>
2026-07-13 11:18:33 +0600

    typecasting

---
+++ module1/lesson3/activity.py
@@ -0,0 +1,15 @@
+x = 32
+y = 7.8789
+name = "sushmoy"
+isStudent = True
+
+#Type of data
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(isStudent))
+#typecasting
+y = int(y)
+print(f"The value of y after typecasting is:{y}.\nThe data type of y is{type(y)}")
+x = float(x)
+print(f"The value of x after typecasting is:{x}.\nThe data type of x is{type(x)}")
