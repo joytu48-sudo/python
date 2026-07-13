3dd5f0fa112d3d38117aa80f36166f3f7eba0ca7
joytu48-sudo <joytu48@gmail.com>
2026-07-13 11:38:09 +0600

    indexing

--- module1/lesson3/activity.py
+++ module1/lesson3/activity.py
@@ -13,3 +13,17 @@ y = int(y)
 print(f"The value of y after typecasting is:{y}.\nThe data type of y is{type(y)}")
 x = float(x)
 print(f"The value of x after typecasting is:{x}.\nThe data type of x is{type(x)}")
+
+word1 = input("enter your word:")
+
+#indexing
+print(f"first charecter is:{word1[0]}")
+print(f"last charecter is:{word1[-1]}")
+#slicing
+print(f"after slicing:{word1[0:4]}")
+#Concatenation
+word2 = " is a coder"
+word3 = word1+word2
+print(word3)
+#reversing
+print(f"after reversing:{word1[::-1]}")
