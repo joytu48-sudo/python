f7128c7b40cda603bd8778873fe46f5f4c520449
joytu48-sudo <joytu48@gmail.com>
2026-08-22 11:30:44 +0600

    factorial

---
+++ module3/lesson2/activity3.py
@@ -0,0 +1,7 @@
+def factorial(number):
+    if number==1 or number==0:
+        return 1
+    else:
+        return number * factorial(number-1)
+number = int(input("Enter your number: "))
+print(factorial(number))
