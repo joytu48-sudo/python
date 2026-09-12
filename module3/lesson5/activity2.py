16e7c2e6655c726ae4b38bab04f40ce0b9fd5611
joytu48-sudo <joytu48@gmail.com>
2026-09-12 11:39:47 +0600

    math

---
+++ module3/lesson5/activity2.py
@@ -0,0 +1,20 @@
+import math
+
+#square root
+number = math.sqrt(25)
+print(number)
+
+a = 1.68
+print(f"floor value of {a} is {math.floor(a)}")
+print(f"ceil value of {a} is {math.ceil(a)}")
+
+#copysign of x and y
+x = 78
+y = -56
+print(math.copysign(x, y))
+
+#greatest common divisor (GDC)
+print(f"GDC of 24 86 is {math.gcd(24,86)}")
+
+#factorial
+print(f"factorial of 5 is {math.factorial(5)}")
